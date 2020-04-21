fc816210c8d3cfe728e8194896dab7ecee275f8d
vedransehovic <vedran.sehovic@gmail.com>
2020-04-20 21:46:09 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,22 @@
 # Add  code here!
+def prime?(n)
+# Corner case 
+    if n <= 1
+        puts "number is not a valid number. (and not a prime)"
+        return false
+    end
+    # Check from 2 to n-1 
+    i=2
+    while i < n
+        if n % i == 0
+            return false
+        else
+            i += 1
+        end
+    end
+    true
+end
+
+
+#enter integer to check if it's prime.
+puts prime?(-5)
