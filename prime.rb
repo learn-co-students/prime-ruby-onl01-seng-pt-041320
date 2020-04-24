179d23bcc8fa91997c9a8c32940b16f1e6ce2569
Jonah Mallard <benjaminjmallard@gmail.com>
2020-04-24 22:53:52 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
-# Add  code here!
+def prime?(number)
+  
+  return false if !number.integer?
+    
+    return false if number < 2 
+      
+      return true if number == 2 
+  
+     (2..number-1).each {|int| return false if number % int == 0}
+    
+    true
+end 
+
+puts prime?(1)
+puts prime?(2)
+puts prime?(11)
+puts prime?(5)
