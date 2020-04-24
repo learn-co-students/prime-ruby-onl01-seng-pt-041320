8786c902fdad33060dca78521d44aa66a4f14917
Anmol Kaur <kaur.anmol26@gmail.com>
2020-04-24 18:00:32 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,24 @@
-# Add  code here!
+def prime?(number)
+  start = 2
+  if number > 1
+    range = (start...number).to_a
+    range.none? do |num_to_test| 
+      number % num_to_test == 0
+    end
+  else
+    false
+  end
+end
+
+
+# def prime?(number)
+#   n = 1
+#   if n > 2
+#     true
+#   elsif
+#     n == 3
+#   else
+#     n <= 2
+#   end
+#   prime?
+# end
