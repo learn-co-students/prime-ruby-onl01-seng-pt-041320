a2168cf4f252b4e5beed78ae9d656fdf1cf69212
Christine Zosche <christine.zosche@gmail.com>
2020-04-24 04:22:11 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,22 @@
-# Add  code here!
+def prime?(n)
+ if n <= 0
+   return false
+ elsif n == 1
+   return false
+ elsif n == 2
+   return true
+   
+ else
+  factors = (2..n).to_a
+ 
+  factors.pop
+ 
+  if factors.any?{|number| n % number == 0}
+   return false
+    
+  else
+    return true
+  end 
+ end
+end
+ 
