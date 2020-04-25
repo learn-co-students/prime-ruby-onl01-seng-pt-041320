5c89d2da27b93477a21cef47dbaaadf40001f672
Joey <joey.alzamora@gmail.com>
2020-04-25 22:47:36 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,5 +1,39 @@
 # Add  code here!
-def prime(3)
-  prime_number = 3
-  if prime_number
-end 
+require 'prime'
+
+def prime?(integer)
+ 
+end
+
+
+prime?(1)
+
+
+
+
+# def prime?(integer)
+#   if 
+#     int.prime?(int)
+#     prime?()
+#   else
+#     int.prime?(int)
+#     prime?(int)
+# end
+# end 
+
+
+
+# def is_prime(num)
+#   n = 2
+#   while n < num
+#     return false if num % n == 0
+#     n += 1
+#   end
+#   true
+# end
+
+# is_prime(7)
+# #=> true
+
+# is_prime(4)
+# #=> false
