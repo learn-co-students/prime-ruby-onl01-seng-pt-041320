9f0e85c16fa335bb87d521519b7551e5187d5511
chica25 <patricia@patriciaco.com>
2020-05-31 20:47:26 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,14 +1,14 @@
 # Add  code here!
 def prime?(number)
-    if number <= 1
+    if number == 1
       false
       elsif number == 2 
       true 
     else 
-      (2..number/2).each do |n| 
+      number.each do |n| 
         if number % n == 0 
             false
         end 
-      true
+    #   true
     end 
 end 
